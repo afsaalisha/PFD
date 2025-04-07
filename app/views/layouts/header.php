7f0c028c3eaef13bff4f33ebf3d66f3112903f29
afsaalisha <22ftt1508@student.pb.edu.bn>
2025-04-07 10:39:03 +0800

    head.

---
+++ app/views/layouts/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Pocket Header</title>
+
+</head>
+<body>
+  <header>
+  <div class="logo-section">
+  <img class="logo-icon" src="/PFD/public/images/pocket.png" alt="Logo">
+  <div class="logo-text">Pocket For Developers</div>
+</div>
+    <nav>
+      <a href="#">Documentation</a>
+      <a href="#">Guidelines</a>
+      <a href="#">Pocket Merchant</a>
+      <a href="#">Pocket Partner</a>
+      <a href="#">Go to Merchant Portal</a>
+    </nav>
+  </header>
+</body>
+</html>
---
+++ app/views/layouts/sidebar.php
@@ -0,0 +1,8 @@
+<div class="sidebar">
+  <div class="sidebar-title">Pocket Pay Rest API</div>
+  <div class="sidebar-links">
+    <a href="#">WooCommerce</a>
+    <a href="#">WIX</a>
+    <a href="#">Shopify</a>
+  </div>
+</div>
