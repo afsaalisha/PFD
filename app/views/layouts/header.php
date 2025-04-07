c2276ad45afc3442e973333629c0557d9e597796
afsaalisha <22ftt1508@student.pb.edu.bn>
2025-04-07 10:41:45 +0800

    oopsies

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -2,8 +2,14 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Pocket Header</title>
-
+  <title>Pocket For Developers</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.js"></script>
+    <link rel="stylesheet" href="/PFD/public/css/styles.css">
+    <link rel="stylesheet" href="/PFD/public/css/mobile.css" media="screen and (max-width: 768px)">
+    <script src="/public/js/tabs.js"></script>
+    
 </head>
 <body>
   <header>
