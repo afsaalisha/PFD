ef0d42e278120d8c889e116b7266f081e8b3cfe5
kuutaro <kuutarojay@gmail.com>
2025-04-30 10:41:29 +0800

    dummy data on merchant tab

--- app/views/merchant.php
+++ app/views/merchant.php
@@ -1,6 +1,67 @@
 <?php
-  $hasSidebar = false; // This must come BEFORE header.php
-  include 'layouts/header.php';
+$hasSidebar = false; // This must come BEFORE header.php
+include 'layouts/header.php';
 ?>
+
+<div class="page-container">
+
+  <div class="main-content">
+    <div class="content-header">
+      <h2>Pocket Merchant</h2>
+    </div>
+
+
+
+    <div class="content-section">
+      <h3>I was going to link this to the Pocket Portal</h3>
+      <p>
+        But I'm not so sure as it may not be permitted.
+      </p>
+    </div>
+
+    <div class="content-section">
+      <h3>So enjoy some words slapped onto the page</h3>
+      <p><strong>If thou would read this</strong> – Maybe I'll put in some poems to read.</p>
+      <p><strong>Although they may not be the most appropriate...</strong> – So I'll keep them here just for today.</p>
+      <p><strong>Here's the first one....</strong> – I hope you ready your eyes.</p>
+      <p><strong>For it may have a horrendous ending</strong> – But nonetheless, I thank you for reading through this.</p>
+      <p><strong>I call this.... "If only"</strong> – <br> <br>
+        If only she knew that her smile deepens mine <br>
+        If only she knew how her words affect time <br> <br>
+
+        If only she know that I whisper her name <br>
+        If only she knew how it sounds so profane <br> <br>
+
+        If only she knew how I longed for her touch <br>
+        If only she knew how her hugs meant so much <br> <br>
+
+        If only she knew how I avoided to kiss her <br>
+        If only she knew that I learned she's my sister. <br> <br> <br></p>
+    </div>
+  </div>
+
+  <div class="table-of-contents">
+    <div class="search-bar">
+      <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+        viewBox="0 0 16 16">
+        <path
+          d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+      </svg>
+      <input type="text" placeholder="Search.." />
+    </div>
+
+    <div class="toc-box">
+      <h4>Table Of Contents</h4>
+      <ul>
+        <li> Overview</li>
+        <li> API Endpoints</li>
+        <li> Step-by-Step Guide</li>
+        <li> FAQs & Troubleshooting</li>
+      </ul>
+    </div>
+  </div>
+
+</div>
+</div>
 
 <?php include 'layouts/footer.php'; ?>
