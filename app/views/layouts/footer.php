949d009bc73afaeaaaf00686da2e8d2b3b7242c5
kuutaro <kuutarojay@gmail.com>
2025-05-07 14:00:20 +0800

    swish swish works

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -10,9 +10,17 @@
             }
         });
     });
+
+    function togglePldqDropdown(header) {
+        const content = header.nextElementSibling;
+        const arrow = header.querySelector('.pldq-arrow');
+        content.classList.toggle('open');
+        arrow.style.transform = content.classList.contains('open') ? 'rotate(180deg)' : 'rotate(0deg)';
+    }
 </script>
 
 
 </body>
 <a href="/PFD/home" class="back-to-home">Back to Home</a>
+
 </html>
