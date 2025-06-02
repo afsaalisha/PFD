6cf3ab0418ed44b457a26215ac98c7e325daf8da
afsaalisha <22ftt1508@student.pb.edu.bn>
2025-06-02 09:42:58 +0800

    yep.

--- app/views/void.php
+++ app/views/void.php
@@ -4,6 +4,75 @@
 
     <main class="doc-content">
 
+    <div class="voidz-wrapper">
+      <h1>HOW TO VOID TRANSACTIONS?</h1>
+      <p>Void could be done by Business Managers or Company profiles in the void tab under “Statement”. They could create void pin on the web portal and would later be used in the Pocket Merchant app to void.</p>
+
+      <div class="voidz-step">
+        <h2>Step 1</h2>
+        <img src="../public/images/void/1.png" alt="Step 1 image" />
+        <p>Click on void tab</p>
+      </div>
+
+      <div class="voidz-step">
+        <h2>Step 2</h2>
+        <img src="../public/images/void/2.png" alt="Step 2 image" />
+        <p>Click on “Create New Pin” button</p>
+      </div>
+
+      <div class="voidz-step">
+        <h2>Step 3</h2>
+        <img src="../public/images/void/3.png" alt="Step 3 image part 1" />
+        <img src="../public/images/void/4.png" alt="Step 3 image part 2" />
+        <p>Create 4 digit pin number</p>
+      </div>
+
+      <div class="voidz-step">
+        <h2>Step 4</h2>
+        <img src="../public/images/void/5.png" alt="Step 4 image" />
+        <p>Void pin successfully created would show on the void tab</p>
+      </div>
+
+      <p>Void button will only appear for current transactions on same day. For transactions that has passed one day, voiding are not possible</p>
+
+      <div class="voidz-step">
+        <h2>Step 5</h2>
+        <img src="../public/images/void/6.png" alt="Step 5 image" />
+        <p>Statement tab will show all transactions</p>
+      </div>
+
+      <div class="voidz-step">
+        <h2>Step 6</h2>
+        <img src="../public/images/void/7.png" alt="Step 6 image" />
+        <p>Click on the void action button</p>
+      </div>
+
+      <div class="voidz-step">
+        <h2>Step 7</h2>
+        <img src="../public/images/void/8.png" alt="Step 7 image" />
+        <p>Enter 4 digit pin set up earlier</p>
+      </div>
+
+      <div class="voidz-step">
+        <h2>Step 8</h2>
+        <img src="../public/images/void/9.png" alt="Step 8 image part 1" />
+        <img src="../public/images/void/10.png" alt="Step 8 image part 2" />
+        <p>Enter reason for voiding</p>
+      </div>
+
+      <div class="voidz-step">
+        <h2>Step 9</h2>
+        <img src="../public/images/void/11.png" alt="Step 9 image" />
+        <p>Successful voiding</p>
+      </div>
+
+      <div class="voidz-step">
+        <h2>Step 10</h2>
+        <img src="../public/images/void/12.png" alt="Step 10 image" />
+        <p>Successful void will be shown on void history</p>
+      </div>
+    </div>
+
     </main>
 
     <?php include 'layouts/footer.php'; ?>
