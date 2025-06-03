df89fe4ebe4461fa926a4ceab071e0ca2d2c5290
kuutaro <kuutarojay@gmail.com>
2025-06-03 11:18:04 +0800

    so i didnt need to ../

--- app/views/payment.php
+++ app/views/payment.php
@@ -11,31 +11,31 @@
         <ol>
             <li>
                 <p>Go to terminal to create a new terminal (if there is no already existing terminal, or if you intend to use a separate terminal to accept online web payment)</p>
-                <img src="../public/images/payment/cnt.png" alt="Terminal" class="key-image">
+                <img src="public/images/payment/cnt.png" alt="Terminal" class="key-image">
             </li>
             <li>
                 <p>Go to the ‘Manage Keys’ page & ‘generate new keys’ to get the API key and API salt for the newly created terminal.</p>
-                <img src="../public/images/payment/nct.png" alt="Newly Created Terminal" class="key-image">
+                <img src="public/images/payment/nct.png" alt="Newly Created Terminal" class="key-image">
             </li>
             <li>
                 <p>Select the terminal you want to create the credentials for.</p>
-                <img src="../public/images/payment/ctc.png" alt="Create Credentials" class="key-image">
+                <img src="public/images/payment/ctc.png" alt="Create Credentials" class="key-image">
             </li>
             <li>
                 <p>Go to ‘Manage Payment Link’ page and click on ‘Create new Link’.</p>
-                <img src="../public/images/payment/cnl.png" alt="Create new Link" class="key-image">
+                <img src="public/images/payment/cnl.png" alt="Create new Link" class="key-image">
             </li>
             <li>
                 <p>Select the terminal you have just created and enter a redirect link. This can be your homepage, or a general thank you page for your website.</p>
-                <img src="../public/images/payment/earl.png" alt="Enter Redirect Link" class="key-image">
+                <img src="public/images/payment/earl.png" alt="Enter Redirect Link" class="key-image">
             </li>
             <li>
                 <p>You can now copy the payment link to share to your customer.</p>
-                <img src="../public/images/payment/styc.png" alt="Copy Payment Link" class="key-image">
+                <img src="public/images/payment/styc.png" alt="Copy Payment Link" class="key-image">
             </li>
             <li>
                 <p>You may overview all your created link as well.</p>
-                <img src="../public/images/payment/ocl.png" alt="Overview" class="key-image">
+                <img src="public/images/payment/ocl.png" alt="Overview" class="key-image">
             </li>
         </ol>
 
@@ -46,12 +46,12 @@
         <ol>
             <li>
                 <p>The payment link will lead to a page that allows customer to manually enter the amount they want to pay to the merchant (note, it cannot be less than $1.00).</p>
-                <img src="../public/images/payment/upl1.png" alt="Enter Amount" class="key-image">
+                <img src="public/images/payment/upl1.png" alt="Enter Amount" class="key-image">
             </li>
             <li>
                 <p>When customer click proceed to payment page, they will be redirected to the Pocket Pay gateway.</p>
-                <img src="../public/images/payment/upl2.png" alt="Payment Page" class="key-image">
-                <img src="../public/images/payment/upl3.png" alt="Pocket Pay Gateway" class="key-image">
+                <img src="public/images/payment/upl2.png" alt="Payment Page" class="key-image">
+                <img src="public/images/payment/upl3.png" alt="Pocket Pay Gateway" class="key-image">
             </li>
         </ol>
 
