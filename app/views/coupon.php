60abc95198f874098f147ead26916c6081c24a65
kuutaro <kuutarojay@gmail.com>
2025-06-10 14:22:14 +0800

    i am a man, woman

--- app/views/coupon.php
+++ app/views/coupon.php
@@ -2,7 +2,60 @@
 <div class="page-container">
     <?php include 'layouts/merchant-sidebar.php'; ?>
 
-    <main class="doc-content">
+    <main class="poc-content">
+
+        <div class="poc-content-header">
+            <h2>Cancelling Coupon</h2>
+        </div>
+
+        <ul>
+            <li>
+                <p><strong>Can be done anytime.</strong></p>
+            </li>
+            <li>
+                <p><strong>Not reversible.</strong></p>
+            </li>
+            <li>
+                <p>Must provide <strong class="red">reason for cancellation</strong>.</p>
+            </li>
+            <li>
+                <p><strong><em>Refunds</em></strong></p>
+                <ul>
+                    <li>
+                        <p>Valid for <strong>organizational, non-Pocket Merchants</strong> that have <strong><em>paid the upfront fee.</em></strong></p>
+                    </li>
+                    <li>
+                        <p><strong>Registered merchants</strong> will <strong><em>not be given a refund</em></strong> as there is <em>no upfront payment to be made.</em></p>
+                    </li>
+                </ul>
+                <img src="public/images/coupon/ccpn.png" alt="Cancelling Coupon" class="poc-image" style="width: 40%;">
+
+            </li>
+        </ul>
+
+        <div class="poc-content-header">
+            <h2>Coupon Dispensing (WIP)</h2>
+        </div>
+
+        <ul>
+            <li>
+                <p>Create coupon --> pending (status tab) --> reviewed by Pocket Admin --> once approved, moved from "status" to "active tab".</p>
+                <ul>
+                    <li>
+                        <p>push coupon <strong>if distribution period has begun</strong></p>
+                    </li>
+                    <li>
+                        <p>"action" tab will change from a Notice to a "push" button</p>
+                        <ul>
+                            <li>
+                                <p>redirected to dispense coupon page</p>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <!-- <img src="public/images/payment/upl1.png" alt="Enter Amount" class="poc-image"> -->
+            </li>
+        </ul>
 
     </main>
 
--- app/views/keys.php
+++ app/views/keys.php
@@ -2,45 +2,45 @@
 <div class="page-container">
     <?php include 'layouts/merchant-sidebar.php'; ?>
 
-    <main class="key-content">
+    <main class="poc-content">
 
-        <div class="content-header">
+        <div class="poc-content-header">
             <h2>How to Generate New Keys</h2>
         </div>
 
         <ol>
             <li>
                 <p>Log in to the <strong>Pocket Merchant Portal.</strong></p>
-                <img src="../public/images/keys/pmp.png" alt="Pocket Merchant Portal" class="key-image">
+                <img src="../public/images/keys/pmp.png" alt="Pocket Merchant Portal" class="poc-image">
             </li>
             <li>
                 <p>Navigate to the <strong>Terminal</strong> page.</p>
-                <img src="../public/images/keys/tp.png" alt="Terminal Page" class="key-image">
+                <img src="../public/images/keys/tp.png" alt="Terminal Page" class="poc-image">
             </li>
             <li>
                 <p>Create a new Terminal Slot. Each slot indicates a different payment terminal, which will be reflected in the payment receipt of your client.<strong> Make sure each terminal ID is unique.</strong></p>
 
-                <img src="../public/images/keys/ts.png" alt="Terminal Slot" class="key-image">
+                <img src="../public/images/keys/ts.png" alt="Terminal Slot" class="poc-image">
             </li>
             <li>
                 <p>Make sure to assign your phone number to the newly created terminal.</p>
-                <img src="../public/images/keys/apn.png" alt="Assign Phone Number" class="key-image">
+                <img src="../public/images/keys/apn.png" alt="Assign Phone Number" class="poc-image">
             </li>
             <li>
                 <p>Then, navigate to the <strong>Manage Keys</strong> page. Your API salt & API key will be shown here once created.</p>
-                <img src="../public/images/keys/mkp.png" alt="Manage Keys Page" class="key-image">
+                <img src="../public/images/keys/mkp.png" alt="Manage Keys Page" class="poc-image">
             </li>
             <li>
                 <p>Click on <strong>Generate New Keys.</strong></p>
-                <img src="../public/images/keys/gnk.png" alt="Generate New Keys" class="key-image">
+                <img src="../public/images/keys/gnk.png" alt="Generate New Keys" class="poc-image">
             </li>
             <li>
                 <p>Select the terminal you created and then click on <strong>Generate.</strong></p>
-                <img src="../public/images/keys/stag.png" alt="Select Terminal and Generate" class="key-image">
+                <img src="../public/images/keys/stag.png" alt="Select Terminal and Generate" class="poc-image">
             </li>
             <li>
                 <p>You have successfully created your API salt & key pair! (If you encounter any error during this process please contact us at <italic>+673 8888222</italic> or through our email <italic>support@threegmedia.com</italic>)</p>
-                <img src="../public/images/keys/kgs.png" alt="Keys Generated Successfully" class="key-image">
+                <img src="../public/images/keys/kgs.png" alt="Keys Generated Successfully" class="poc-image">
             </li>
         </ol>
 
