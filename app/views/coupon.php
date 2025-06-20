ea4de88c5911ac6d429135369b28b6f0e255dd60
kuutaro <kuutarojay@gmail.com>
2025-06-20 12:13:18 +0800

    Imported images for coupon dispensing

--- app/views/coupon.php
+++ app/views/coupon.php
@@ -40,22 +40,26 @@
         <ul>
             <li>
                 <p>Create coupon --> pending (status tab) --> reviewed by Pocket Admin --> once approved, moved from "status" to "active tab".</p>
+                <img src="/PFD/public/images/coupon/pendingreview.png" alt="Pending Review" class="poc-image-small" onclick="openModal(this.src)">
                 <ul>
                     <li>
                         <p>push coupon <strong>if distribution period has begun</strong></p>
+                        <img src="/PFD/public/images/coupon/distributionperiod.png" alt="Distribution Period" class="poc-image-small" onclick="openModal(this.src)">
                     </li>
                     <li>
                         <p>"action" tab will change from a Notice to a "push" button</p>
+                        <img src="/PFD/public/images/coupon/actiontab.png" alt="Action tab" class="poc-image-small" onclick="openModal(this.src)">
                         <ul>
                             <li>
                                 <p>redirected to dispense coupon page</p>
+                                <img src="/PFD/public/images/coupon/redirect.png" alt="Redirect to Dispense Coupon" class="poc-image-small" onclick="openModal(this.src)">
                             </li>
                         </ul>
                     </li>
                 </ul>
-                <!-- <img src="public/images/payment/upl1.png" alt="Enter Amount" class="poc-image"> -->
-            </li>
 
+            </li>
+            <img src="/PFD/public/images/coupon/quantity.png" alt="Quantity" class="poc-image-small" onclick="openModal(this.src)">
             <li>
                 <p>Starting coupon quantitiy</p>
             </li>
@@ -67,6 +71,7 @@
             </li>
             <li>
                 <p>Coupon dispenser could be done by entering</p>
+                <img src="/PFD/public/images/coupon/manualorcsv.png" alt="Enter Phone Number or Upload CSV file" class="poc-image-small" onclick="openModal(this.src)">
                 <ul>
                     <li>
                         <p>User phone number manually or;</p>
@@ -75,11 +80,17 @@
                         <p>CSV file upload</p>
                     </li>
                 </ul>
+                <img src="/PFD/public/images/coupon/checkbox.png" alt="Checkbox to allow reusable coupons" class="poc-image-small" onclick="openModal(this.src)">
                 <p><em>*Only tick checkbox to allow reusable coupons. otherwise, leave it unticked.</em></p>
+
+                <img src="/PFD/public/images/coupon/pocketusers.png" alt="Pocket Users" class="poc-image-small" onclick="openModal(this.src)">
                 <p><em>*Only Pocket users can receive & use Pocket coupons.</em></p>
+
                 <ul>
                     <li>Better if merchants encourage customers to use Pocket for coupons.</li>
                 </ul>
+
+                <img src="/PFD/public/images/coupon/archivedtab.png" alt="Archive tab" class="poc-image-small" onclick="openModal(this.src)">
                 <p><em>*Once all coupons (qty) are dispensed & fully redeemed, coupon will be moved to the "Archive" tab.</em></p>
                 <ul>
                     <li>If qty is unlimited, then it will be moved to the "Archive" tab once it reaches the 'end dispensing date'.</li>
